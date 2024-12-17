9635f8d61a117a3f39d00f98b6b830165318e106
Aniket Nagpure <aniketnagpure28@gmail.com>
2024-12-18 00:01:20 +0530

    create and get Lecture route created with lecture model

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -3,6 +3,8 @@ import { deleteMediaFromCloudinary, uploadMedia } from "../utils/cloudinary.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs/promises";
+import { Lecture } from "../models/lecture.model.js";
+import { resolveSoa } from "dns";
 
 // Define __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -139,6 +141,68 @@ export const getCourseById = async (req, res) => {
             course
         })
     } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to create course",
+        });
+    }
+}
+
+// Lecture API's
+
+export const createLecture = async(req,res) => {
+    try {
+       const { lectureTitle } = req.body;
+       const { courseId } = req.params;
+       
+       if(!lectureTitle || !courseId) {
+            return res.status(404).json({
+                success: false,
+                message: "Lecture Title and is required"
+            })
+       }
+
+       // create lecture
+       const lecture = await Lecture.create({lectureTitle});
+
+       const course = await Course.findById(courseId);
+       if(course){
+            course.lectures.push(lecture._id);
+            await course.save();
+       }
+
+       return res.status(201).json({
+            success: true,
+            message: "Lecture created successfully",
+            lecture
+       })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to create course",
+        });
+    }
+}
+
+export const getCourseLecture = async(req, res) => {
+    try{
+        const {courseId} = req.params;
+        
+        const course  = await Course.findById(courseId).populate('lectures');
+        if(!course){
+            return res.status(404).json({
+                success: false,
+                message: 'Course not found'
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: 'Course lectures found successfully',
+            lectures: course.lectures
+        })
+    }catch(error){
         console.log(error);
         return res.status(500).json({
             success: false,
--- routes/course.route.js
+++ routes/course.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import isAuthenticated from '../middleware/isAuthenticated.js';
-import { createCourse, editCourse, getCourseById, getCreatorCourses } from '../controllers/course.controller.js';
+import { createCourse, createLecture, editCourse, getCourseById, getCourseLecture, getCreatorCourses } from '../controllers/course.controller.js';
 import upload from '../utils/multer.js';
 
 const router = express.Router();
@@ -12,5 +12,9 @@ router.route("/creator-courses").get(isAuthenticated, getCreatorCourses);
 router.route("/:courseId").put(isAuthenticated, upload.single('courseThumbnail'), editCourse);
 
 router.route("/:courseId").get(isAuthenticated, getCourseById);
+
+router.route("/:courseId/lecture").post(isAuthenticated, createLecture);
+
+router.route("/:courseId/lecture").get(isAuthenticated, getCourseLecture);
 
 export default router;
